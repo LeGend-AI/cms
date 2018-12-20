655167e2f40c7c5434c1d06d8eb2b3e5d33a0aeb
LeGendAI <joseph04912654@gmail.com>
2018-12-20 16:46:15 +0800

    account

--- cms/cms/urls.py
+++ cms/cms/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path(r'/^member/', include('members.urls')),
-    path(r'/', include('members.urls')),
-    path(r'/about', include('members.urls')),
+    path(r'members/', include('members.urls')),
+    path(r'', TemplateView.as_view(template_name="index.html")),
+    path(r'about', TemplateView.as_view(template_name="about.html")),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- cms/members/urls.py
+++ cms/members/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "members"
 
 urlpatterns = [
-  path('/^members', views.MemberListView.as_view(), name='list'),
-  path('/^sign_up', views.SignUpView.as_view(), name='sign_up'),
+  path('', views.MemberListView.as_view(), name='list'),
+  path('sign_up', views.SignUpView.as_view(), name='sign_up'),
 ]
